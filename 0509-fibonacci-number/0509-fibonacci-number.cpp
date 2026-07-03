46e87b10e946c7f69ad11631f37ff26e2abd1c87
gulmohargautam2005-bit <gulmohargautam2005@gmail.com>
2026-07-03 12:04:26 +0530

    Time: 119 ms (22.88%), Space: 8.6 MB (10.66%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fibbo(int n,unordered_map <int,int> &dp)
+    {
+       
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        if(dp.find(n)!=dp.end())
+            dp[n];
+        int c1 = fibbo(n-1,dp);
+        int c2 = fibbo(n-2,dp);
+        dp[n-1]=c1;
+        dp[n-2]=c2;
+        return c1+c2;
+    }
+    int fib(int n) {
+        unordered_map <int,int> dp;
+        return fibbo(n,dp);
+    }
+};
