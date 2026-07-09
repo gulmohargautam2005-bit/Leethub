57a4b7e8c5e25e80264ba606f25a0f95712aad55
gulmohargautam2005-bit <gulmohargautam2005@gmail.com>
2026-07-09 14:43:47 +0530

    Time: 13 ms (10.71%), Space: 24 MB (7.55%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        unordered_map<int,int> f;
+        int sum =0;
+        for(int i =0;i<n;i++)
+        {
+            sum = sum +nums[i];
+            f[nums[i]]++;
+            if(f[nums[i]]>1)
+                sum = sum -nums[i]*f[nums[i]];
+        }        
+        return sum;
+    }
+};
