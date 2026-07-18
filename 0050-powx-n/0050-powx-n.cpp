892d49be479cd774caa22d4e25820a626179095d
gulmohargautam2005-bit <gulmohargautam2005@gmail.com>
2026-07-18 18:37:57 +0530

    Time: 0 ms (100%), Space: 8.7 MB (12.26%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
+    double fun(double x,int n)
+    {
+        if(n<0)
+        {
+            return (1/(x*fun(x,(n+1)*-1)));
+        }
+        if(n==0)
+        {
+            return 1;
+        }
+        if(n%2==0)
+        {
+            
+            return pow(x,n/2)*fun(x,n/2);
+            
+        }
+        if(n%2!=0)
+        {
+            return x*pow(x,n/2)*fun(x,n/2);
+        }
+
+        return x*fun(x,n-1);
+    }
     double myPow(double x, int n) {
-        return(pow(x,n));
+        return fun(x,n);
     }
 };
