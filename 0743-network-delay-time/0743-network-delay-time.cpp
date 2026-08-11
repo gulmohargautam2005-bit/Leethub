00f3743c0d8f9481be4072c73ec1284696759dac
gulmohargautam2005-bit <gulmohargautam2005@gmail.com>
2026-08-11 07:17:11 +0000

    Time: 67 ms (96.87%), Space: 44.4 MB (68.76%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,56 +1,57 @@
 class Solution {
 public:
-void bfs(vector<vector<pair<int,int>>> &res,priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>&pq,vector <int> &dist)
-  {
-      while(!pq.empty())
-      {
-          pair <int,int>p =pq.top();
-          int d=p.first;
-          int node=p.second;
-          pq.pop();
-          if(d>dist[node])
-              {
-                  continue;
-              }
-          for(int i=0;i<res[node].size();i++)
-          {
-              int neigh = res[node][i].first;
-              int weight =res[node][i].second;
-          
-              if((d+weight)<dist[neigh])
-              {
-                  dist[neigh]=d+weight;
-                  pq.push({d+weight,neigh});
-              }
-          }
-      }
-  }
+    void dfs(vector<vector<pair<int,int>>>& res,priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>& pq,vector<int>&dest)
+    {
+        while(!pq.empty())
+        {
+            pair<int,int> p =pq.top();
+            pq.pop();
+            int d= p.first;
+            int node = p.second;
+            if(d>dest[node])
+            {
+                continue;
+            }
+            for(int i=0;i<res[node].size();i++)
+            {
+                int neigh=res[node][i].first;
+                int weight =res[node][i].second;
+                if((d+weight)<dest[neigh])
+                {
+                    dest[neigh]=d+weight;
+                    pq.push({d+weight,neigh});
+                }
+            }
+
+        }
+    }
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        int nn = times.size();
         vector<vector<pair<int,int>>> res(n+1);
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        vector <int> distance (n+1,INT_MAX);
-        for(int i =0;i<times.size();i++)
+        vector <int> distance(n+1,INT_MAX);
+        for(int i=0;i<times.size();i++)
         {
-            int src= times[i][0];
-            int dest = times[i][1];
-            int weigh = times[i][2];
-            res[src].push_back({dest,weigh});
-          
+            int src = times[i][0];
+            int dst= times[i][1];
+            int weight = times[i][2];
+            res[src].push_back({dst,weight});
         }
+        distance[k]=0;
         pq.push({0,k});
-        distance[k] = 0;
-        bfs(res,pq,distance);
-        int maxi=INT_MIN;
-        for(int i=1;i<n+1;i++)
+        dfs(res, pq, distance);
+        int ans = INT_MIN;
+        for(int i=1;i<=n;i++)
         {
             if(distance[i]==INT_MAX)
             {
                 return -1;
             }
-            maxi= max(maxi,distance[i]);
+            else
+            {
+                ans = max(ans,distance[i]);
+            }
         }
-        return maxi;
+        return ans;
     }
-        
-    
 };
