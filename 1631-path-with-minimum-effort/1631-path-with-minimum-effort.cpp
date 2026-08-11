9293af1286d734bd21ce5d59c9231b7e69d11367
gulmohargautam2005-bit <gulmohargautam2005@gmail.com>
2026-08-11 09:12:16 +0000

    Time: 67 ms (35.73%), Space: 24.4 MB (91.99%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,56 +1,48 @@
 class Solution {
 public:
-   bool valid(int r,int c,int n,int m)
-   {
-    if(r<0 || r>=n || c<0 || c>=m)
+    bool check(int &r,int &c,int &n,int&m)
     {
-        return false;
+        if(r<0 || r>=n || c<0 || c>=m)
+            return false;
+        return true;
     }
-    return true;
-   }
-   int bfs(vector<vector<int>> &res,priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>&pq,int &n,int &m,vector<vector<int>> &heights,int (&x)[],int (&y)[])
-   {
-      while(!pq.empty())
-      {
-          pair<int, pair<int,int>> p = pq.top();
-          int d=p.first;
-          int row=p.second.first;
-          int col = p.second.second;
-          pq.pop();
-          
-          for(int k=0;k<4;k++)
-          {
-            int r = row +x[k];
-            int c =col +y[k];
-            if(!valid(r,c,n,m))
-                continue;
-            int diff = abs(heights[row][col]-heights[r][c]);
-            int absmax =max(d,diff);
-            if(absmax<res[r][c])
+    int dfs(priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>&pq,vector<vector<int>>&res,int (&x)[],int (&y)[],int &n,int &m,vector<vector<int>>& heights)
+    {
+        while(!pq.empty())
+        {
+            pair<int,pair<int,int>> p =pq.top();
+            pq.pop();
+            int d = p.first;
+            int row = p.second.first;
+            int col = p.second.second;
+            for(int i =0;i<4;i++)
             {
-            res[r][c]=absmax;
-              pq.push({absmax,{r,c}});
+                int r = row+x[i];
+                int c = col +y[i];
+                if(!check(r,c,n,m))
+                {
+                    continue;
+                }
+                int diff = abs(heights[row][col]-heights[r][c]);
+                int absmax = max(d,diff);
+                if(absmax<res[r][c])
+                {
+                    res[r][c]=absmax;
+                    pq.push({absmax,{r,c}});
+                }
             }
-    
-          }
-      }
-      return res[n-1][m-1];
-      
-  }
+        }
+        return res[n-1][m-1];
+    }
     int minimumEffortPath(vector<vector<int>>& heights) {
-        int x[4]={1,-1,0,0};
+        int x[4] ={1,-1,0,0};
         int y[4]={0,0,1,-1};
-        int n =heights.size();
-        int m =heights[0].size();
-        vector<vector<int>> res(n, vector<int>(m, INT_MAX));
-
-       priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+        int n = heights.size();
+        int m = heights[0].size();
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        vector<vector<int>> res(n,vector <int> (m,INT_MAX));
         res[0][0] = 0;
         pq.push({0,{0,0}});
-        
-    
-        return bfs(res,pq,n,m,heights,x,y);
+        return dfs(pq,res,x,y,n,m,heights);
     }
-        
-        
 };
